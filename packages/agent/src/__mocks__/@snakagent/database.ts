6f3608ca5f0a753b7f4a0fb2ae12c08f1cedb12c
HxSimo <HxSimo@proton.me>
2025-08-11 13:11:25 -0300

    update mock databse

--- packages/agent/src/__mocks__/@snakagent/database.ts
+++ packages/agent/src/__mocks__/@snakagent/database.ts
@@ -1,10 +1,10 @@
 export class Query {
-  constructor(public sql: string, public params: any[] = []) {}
+  constructor(public sql: string, public params: unknown[] = []) {}
 }
 
 export class Postgres {
   static query = jest.fn();
-  static Query = jest.fn().mockImplementation((sql: string, params: any[] = []) => {
+  static Query = jest.fn().mockImplementation((sql: string, params: unknown[] = []) => {
     return new Query(sql, params);
   });
 } 
