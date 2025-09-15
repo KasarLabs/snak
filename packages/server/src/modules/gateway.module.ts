7625d45db4a127a9195378f0b849cfdb12efa921
HxSimo <HxSimo@proton.me>
2025-09-15 01:40:35 -0300

    gateway module update

--- packages/server/src/modules/gateway.module.ts
+++ packages/server/src/modules/gateway.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MyGateway } from '../controllers/gateway.controller.js';
-import { ConfigurationService } from '../../config/configuration.js';
+import { ConfigModule } from '../../config/config.module.js';
 import { AgentsModule } from './agents.module.js';
 
 @Module({
-  imports: [AgentsModule],
-  providers: [MyGateway, ConfigurationService],
+  imports: [AgentsModule, ConfigModule],
+  providers: [MyGateway],
 })
 export class GatewayModule {}
