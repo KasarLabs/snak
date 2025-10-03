27cee1cf6821df721a831a3be7358f7406dd3bd3
Etienne Duclos <ledude2707@gmail.com>
2025-10-03 16:57:00 +0200

    move task-parser

--- packages/agent/src/agents/graphs/sub-graph/task-manager.graph.ts
+++ packages/agent/src/agents/graphs/sub-graph/task-manager.graph.ts
@@ -21,9 +21,7 @@ import { ChatPromptTemplate } from '@langchain/core/prompts';
 import { AnyZodObject } from 'zod';
 import { AgentConfig, logger } from '@snakagent/core';
 import { GraphConfigurableAnnotation, GraphState } from '../graph.js';
-import {
-  TaskManagerNode,
-} from '../../../shared/enums/agent.enum.js';
+import { TaskManagerNode } from '../../../shared/enums/agent.enum.js';
 import {
   DynamicStructuredTool,
   StructuredTool,
@@ -41,62 +39,7 @@ import {
 } from '@prompts/agents/task-manager.prompts.js';
 import { TaskManagerToolRegistryInstance } from '../tools/task-manager.tools.js';
 import { GraphError } from '../utils/error.utils.js';
-
-export function tasks_parser(
-  tasks: TaskType[],
-  isHumanTask: boolean = false
-): string {
-  try {
-    if (!tasks || tasks.length === 0) {
-      return '<tasks-history>\n  <!-- No tasks available -->\n</tasks-history>';
-    }
-
-    // Filter tasks based on isHumanTask parameter
-    let filteredTasks = tasks;
-    if (isHumanTask) {
-      // For human tasks, only include tasks that have human steps or human responses
-      filteredTasks = tasks.filter(
-        (task) =>
-          task.isHumanTask ||
-          task.human ||
-          (task.steps && task.steps.some((step) => step.type === 'human'))
-      );
-    }
-
-    const formattedTasks: string[] = [];
-    filteredTasks.forEach((task) => {
-      const escapeXML = (str: string) => {
-        return str
-          .replace(/&/g, '&amp;')
-          .replace(/</g, '&lt;')
-          .replace(/>/g, '&gt;')
-          .replace(/"/g, '&quot;')
-          .replace(/'/g, '&apos;');
-      };
-
-      formattedTasks.push(
-        `  <task name="${task.task?.directive}" id="${task.id}"">`
-      );
-      formattedTasks.push(`    <status>${escapeXML(task.status)}</status>`);
-      if (task.task_verification) {
-        formattedTasks.push(
-          `    <verification_result>${escapeXML(task.task_verification)}</verification_result>`
-        );
-      } else if (task.human) {
-        formattedTasks.push(
-          `    <ai_request>${escapeXML(JSON.stringify(task.thought.speak))}</ai_request>`
-        );
-        formattedTasks.push(
-          `    <human_response>${escapeXML(task.human)}</human_response>`
-        );
-      }
-      formattedTasks.push(`</task>`);
-    });
-    return formattedTasks.join('\n');
-  } catch (error) {
-    throw error;
-  }
-}
+import { tasks_parser } from '../parser/tasks/tasks.parser.js';
 
 export const parseToolsToJson = (
   tools: (StructuredTool | Tool | DynamicStructuredTool<AnyZodObject>)[]
